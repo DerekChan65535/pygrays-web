77083932de796889b3adabf906f7d522de6d86f4
Derek <deckerchan@gmail.com>
2025-04-27 23:26:34 +1000

    feat: add initial structure for Inventory page with TypeScript and SCSS

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,5 +1,14 @@
-const Inventory = () => {
+import React from "react";
+
+interface InventoryProps {
+}
+
+function Inventory(props: InventoryProps): React.JSX.Element {
+    
+    
+    
+    
     return <h1>Inventory</h1>;
-};
+}
 
 export default Inventory;
