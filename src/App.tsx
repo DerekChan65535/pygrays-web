9d59fb376c6a03d60d70dbaa39fd93585d246f9e
Derek <deckerchan@gmail.com>
2025-04-27 23:33:26 +1000

    refactor: reorganize page components and update import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Inventory from "./pages/Inventory";
+import Home from "./pages/home/Home";
+import Inventory from "./pages/inventory/Inventory";
 import Layout from "./Layout";
 
 function App() {
