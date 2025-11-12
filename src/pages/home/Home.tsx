7405f4201f9a8e5828f3bc1b861f3c8567725ef7
Derek <deckerchan@gmail.com>
2025-11-12 11:18:02 +1100

    feat: enhance Home page layout with image addition and improve visual presentation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-    return <h1>Home Page</h1>;
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <img 
+                src="/images/image.avif" 
+                alt="Home page image" 
+                style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }}
+            />
+        </div>
+    );
 };
 
 export default Home;
